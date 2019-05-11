863580c579ef5b38da62fb76b67253ddf208dd17
Callum <s5119461@bournemouth.ac.uk>
2019-05-11 13:49:17 +0100

    Initial Commit

---
+++ Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody rb;
+	public float speed = 5.0f;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+		
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			rb.velocity = transform.right * speed; 
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			rb.velocity = -transform.right * speed;
+		}
+	}
+}
